33a5629cf69d89bef281205644202d8ab6373e97
Tanaka Arata <ky.37f.9850@s.thers.ac.jp>
2025-12-18 00:56:19 +0900

    feat: edit eval sh

--- eval.sh
+++ eval.sh
@@ -16,8 +16,20 @@ REPR_TYPE="stacked_histogram"  # stacked_histogram voxel_grid
 NORM="norm"
 
 DATA_DIR="/path/to/dataset/"
-CKPT="/path/to/checkpoint.ckpt"
+GROUP=""
 
+CKPT="/path/to/checkpoint.ckpt"
+TRAIN_TASK="detection" # detection optical_flow
+
+if [ "$TRAIN_TASK" == "detection" ]; then
+    PROJECT_PREFIX="E-Det"
+elif [ "$TRAIN_TASK" == "optical_flow" ]; then
+    PROJECT_PREFIX="E-Flow"
+else
+    echo "Unsupported TRAIN_TASK: $TRAIN_TASK"
+    exit 1
+fi
+PROJECT="${PROJECT_PREFIX}_${MODEL}_${DATASET}"
 
 REPR_NAME="${REPR_TYPE}_dt=${DT_MS}_nbins=${T_BIN}"
 
@@ -40,4 +52,4 @@ hardware.num_workers.eval=${EVAL_WORKERS_PER_GPU} \
 batch_size.train=${BATCH_SIZE_PER_GPU} \
 batch_size.eval=${BATCH_SIZE_PER_GPU} \
 dataset.downsample_by_factor_2=${DOWNSAMPLE} \
-checkpoint="'${CKPT}'" \
++train_task=${TRAIN_TASK}
