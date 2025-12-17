157364e66f36d49056f72d65b347654ce9a2de91
Tanaka Arata <ky.37f.9850@s.thers.ac.jp>
2025-12-18 00:34:09 +0900

    feat: add eval.sh

---
+++ eval.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# GPU設定
+GPU_IDS=0  
+MODEL="rvt"  # rvt, yolox_lstm
+DATASET="SEVD"  # gen1 gen4 VGA SEVD
+BATCH_SIZE_PER_GPU=12
+EVAL_WORKERS_PER_GPU=4
+DT_MS=50
+T_BIN=10
+CHANNEL=20
+SEQUENCE_LENGTH=5
+PROJECT="E-Det_${MODEL}_${DATASET}"
+DOWNSAMPLE=true  # true false
+REPR_TYPE="voxel_grid"  # stacked_histogram voxel_grid 
+NORM="norm"
+
+DATA_DIR="/path/to/dataset/"
+GROUP=""
+CKPT="/path/to/checkpoint.ckpt"
+
+
+REPR_NAME="${REPR_TYPE}_dt=${DT_MS}_nbins=${T_BIN}"
+
+if [ "$REPR_TYPE" == "voxel_grid" ]; then
+    REPR_NAME="${REPR_NAME}_${NORM}"
+fi
+
+echo "Generated Representation Name: ${REPR_NAME}"
+
+python3 validation.py \
+dataset=${DATASET} \
+model=${MODEL} \
+dataset.path=${DATA_DIR} \
+dataset.ev_repr_name="'${REPR_NAME}'" \
+dataset.sequence_length=${SEQUENCE_LENGTH} \
+hardware.gpus=${GPU_IDS} \
+model.backbone.input_channels=${CHANNEL} \
+hardware.num_workers.eval=${EVAL_WORKERS_PER_GPU} \
+batch_size.eval=${BATCH_SIZE_PER_GPU} \
+dataset.downsample_by_factor_2=${DOWNSAMPLE} \
+checkpoint="'${CKPT}'" \
