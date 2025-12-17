55965a008d3d5315dd206e0beeb51e0efc2d25c5
Tanaka Arata <ky.37f.9850@s.thers.ac.jp>
2025-12-18 00:37:01 +0900

    feat: fix eval.sh

--- eval.sh
+++ eval.sh
@@ -5,18 +5,17 @@ GPU_IDS=0
 MODEL="rvt"  # rvt, yolox_lstm
 DATASET="SEVD"  # gen1 gen4 VGA SEVD
 BATCH_SIZE_PER_GPU=12
+TRAIN_WORKERS_PER_GPU=8
 EVAL_WORKERS_PER_GPU=4
 DT_MS=50
 T_BIN=10
 CHANNEL=20
 SEQUENCE_LENGTH=5
-PROJECT="E-Det_${MODEL}_${DATASET}"
 DOWNSAMPLE=true  # true false
 REPR_TYPE="voxel_grid"  # stacked_histogram voxel_grid 
 NORM="norm"
 
 DATA_DIR="/path/to/dataset/"
-GROUP=""
 CKPT="/path/to/checkpoint.ckpt"
 
 
@@ -36,7 +35,9 @@ dataset.ev_repr_name="'${REPR_NAME}'" \
 dataset.sequence_length=${SEQUENCE_LENGTH} \
 hardware.gpus=${GPU_IDS} \
 model.backbone.input_channels=${CHANNEL} \
+hardware.num_workers.train=${TRAIN_WORKERS_PER_GPU} \
 hardware.num_workers.eval=${EVAL_WORKERS_PER_GPU} \
+batch_size.train=${BATCH_SIZE_PER_GPU} \
 batch_size.eval=${BATCH_SIZE_PER_GPU} \
 dataset.downsample_by_factor_2=${DOWNSAMPLE} \
 checkpoint="'${CKPT}'" \
